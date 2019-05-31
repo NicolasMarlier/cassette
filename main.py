6fcef16cea84133fcaaf28686696496790273990
Nicolas Marlier <nicolas@juliedesk.com>
2019-05-31 13:46:43 +0200

    Absolute music paths é

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from qrcode import inteprete_picture
 from mouse import listen
 import mutagen.mp3
 import json
+import os
 
 def initialize_music():
     mixer.init()
 
+def fullFilename(filename):
+     dirname = os.path.dirname(__file__)
+     return os.path.join(dirname, filename)
+
 def play_music(token):
     if token == None:
-        playSound("effects/unrecognized.mp3")
+        playSound(fullFilename("effects/unrecognized.mp3"))
         print("No music to play")
         return
     print("playing music", token)
     if token == b'8fad1fc5-203c-4027-916b-07f670d6114a':
-        playSound("music/johnny_b_goode.mp3")
+        playSound(fullFilename("music/johnny_b_goode.mp3"))
     elif token == b'68d26688-907d-4dcb-88c4-47b432fa7e5a':
-        playSound("music/hit_the_road_jack.mp3")
+        playSound(fullFilename("music/hit_the_road_jack.mp3"))
     else:
         print("Unrecognized music")
-        playSound("effects/unrecognized.mp3")
+        playSound(fullFilename("effects/unrecognized.mp3"))
         return
 
 def playSound(filename):
@@ -39,7 +44,7 @@ def stopMusic():
 
 def onClick(pressed):
     if(pressed):
-        playSound("effects/clicked.mp3")
+        playSound(fullFilename("effects/clicked.mp3"))
         activation()
     else:
         stopMusic()
